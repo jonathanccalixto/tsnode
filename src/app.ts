18e55f7b5ef0231faf78410f88e415dde823b456
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2019-08-06 05:11:07 -0300

    Configuring mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 class App {
   public express: express.Application
@@ -7,10 +8,17 @@ class App {
   public constructor () {
     this.express = express()
     this.middlewares()
+    this.database()
   }
 
   private middlewares (): void {
     this.express.use(express.json())
     this.express.use(cors())
+  }
+
+  private database (): void {
+    mongoose.connect('mongodb:localhost:27017/tsnode', {
+      useNewUrlParser: true
+    })
   }
 }
